80c044f159f7427270e97ad378be1fae44d75c84
Ajvr2512 <ajvr2512@gmail.com>
2024-03-15 17:30:56 -0500

    Update package-lock.json

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Modal from 'react-modal';
 import '../index.css';
 
@@ -7,6 +6,12 @@ const ModalConfirmacion = ({ isOpen, onClose, userInfo, onConfirm, setCode }) =>
     onClose();
     setCode('');
   };
+
+  const handleConfirm = (event) => {
+    event.preventDefault();
+    onConfirm();
+  };
+
   const modalStyle = {
     content: {
       maxWidth: '400px',
@@ -14,26 +19,33 @@ const ModalConfirmacion = ({ isOpen, onClose, userInfo, onConfirm, setCode }) =>
       margin: 'auto',
     },
   };
+
   return (
-    <Modal isOpen={isOpen} onRequestClose={onClose} style={modalStyle}>
+    <Modal isOpen={isOpen} onRequestClose={onClickModal} style={modalStyle}>
       {userInfo && (
-        <div className="ModalBody">
-          <div className="modalHeader">
-            <img src="src/assets/img/confirma.png" alt="Imagen" className="modalImage" />
-            <h2 className="titleConfirmacion">Confirmar nombre</h2>
+        <form onSubmit={handleConfirm}>
+          <div className="ModalBody">
+            <div className="modalHeader">
+              <img
+                src="src/assets/img/confirma.png"
+                alt="Imagen"
+                className="modalImage"
+              />
+              <h2 className="titleConfirmacion">Confirmar nombre</h2>
+            </div>
+            <p className="text">
+              ¿Podría confirmar si su nombre es
+              <span className="name"> {userInfo.nombre}</span>
+              <span className="name"> {userInfo.apellido}</span>?
+            </p>
+            <button autoFocus type="submit" className="btnConfirmar">
+              Confirmar
+            </button>
+            <button className="btnNo" onClick={onClickModal}>
+              No, no lo soy
+            </button>
           </div>
-          <p className="text">
-            ¿ Podría confirmar si su nombre es
-            <span className="name"> {userInfo.nombre}</span>
-            <span className="name"> {userInfo.apellido}</span>?
-          </p>
-          <button className="btnConfirmar" onClick={onConfirm}>
-            Confirmar
-          </button>
-          <button className="btnNo" onClick={onClickModal}>
-            No, no lo soy
-          </button>
-        </div>
+        </form>
       )}
     </Modal>
   );
